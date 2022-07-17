4deeb52ae2b45d604c175c4adfeaf260e7f2499a
Kamrul Hasan <kamrul.devcs@gmail.com>
2022-07-17 22:17:30 +0600

    change config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CpaBooks'
+copyright = '2021, CpaBooks'
+author = 'CpaBooks'
 
 release = '0.1'
 version = '0.1.0'
